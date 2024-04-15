60acc3fad094f0c03398a5bc45de5c22dba2b379
Jover Lee <joverlee521@gmail.com>
2024-04-14 22:10:00 -0700

    Add base Deck scene

---
+++ scenes/card_battle/deck.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+signal cards_depleted
+
+@export var image: CompressedTexture2D
+
+var cards: Array = []
+
+
+func _ready():
+	if image != null:
+		$DeckImage.texture = image
+
+	if cards.is_empty():
+		$DeckImage.hide()
+
+
+func add_card(card) -> void:
+	cards.append(card)
+	$DeckImage.show()
+
+
+func remove_card(card: Card) -> void:
+	cards.erase(card)
+
+	if cards.is_empty():
+		$DeckImage.hide()
+		cards_depleted.emit()
+
+
+func shuffle() -> void:
+	cards.shuffle()
