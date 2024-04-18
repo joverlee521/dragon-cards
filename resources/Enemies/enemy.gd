be1e31d26754b37637a4a68b78eb0e72027b1b5c
Jover Lee <joverlee521@gmail.com>
2024-04-17 19:14:19 -0700

    Add base Enemy Resource

---
+++ resources/Enemies/enemy.gd
@@ -0,0 +1,10 @@
+class_name EnemyAttributes extends Resource
+
+
+@export_group("EnemyInfo")
+@export var enemy_name : String
+@export var enemy_sprite: AtlasTexture
+
+
+@export_group("EnemyStats")
+@export var max_health : int
