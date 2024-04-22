05d35ce89abb54639cde3d0b89c8143975d9dab3
Adam de Lorimier-Farmer <adam.a.farmer4129@gmail.com>
2024-04-22 15:56:41 -0700

    Added Damage Targets and Class Stats to card resource

--- resources/cards/card.gd
+++ resources/cards/card.gd
@@ -2,15 +2,22 @@ class_name CardAttributes extends Resource
 
 enum DAMAGE_TYPES {CUTTING, BLUNT, FIRE, ICE, LIGHTNING, WATER, LIGHT, DARK, DEFAULT}
 
+enum TARGET_TYPES {SINGLE, GROUP}
+
 @export_group("CardInfo")
 @export var name : String
 #Consider making type into an enum for categorization. Spell, Physical, Armor e.g.
 @export var DamageTypes : Array[DAMAGE_TYPES]
 
+@export var Target : TARGET_TYPES
+
 @export_group("Stats")
 @export var attack: int
 @export var defense: int
 @export var stamina_cost: int
+
+@export_group ("Class Stats")
+@export var parry_value : int
 
 @export_group("Visual")
 @export var card_background: AtlasTexture
