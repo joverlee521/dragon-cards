463f276397a91a4dd0ef7fc1adc55568490cab9c
Jover Lee <joverlee521@gmail.com>
2024-05-04 17:10:02 -0700

    ShieldBash: Add special effects
    If the enemy is killed by the attack, then the player gains defense.

--- resources/cards/ShieldBash.gd
+++ resources/cards/ShieldBash.gd
@@ -1,5 +1,15 @@
 extends CardAttributes
 
 
-#Add a script here that fires when an enemy is killed with this card.
-#It should give the player 5 defense
+# Override the default effects on card player because the effect depends on
+# the outcome of the attack
+func apply_effects_to_card_player():
+	pass
+
+
+# Add defense to card player if the enemy dies from the attack
+func apply_effects_to_card_targets():
+	super()
+
+	if card_targets.any(func(target): return target.health <= 0):
+		card_player.add_defense(defense)
