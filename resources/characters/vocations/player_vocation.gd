b5968fef8a2450be7cb0ce63b00f41c4ba9ed06e
Jover Lee <joverlee521@gmail.com>
2024-05-17 21:05:25 -0700

    Add base PlayerVocation and Warrior vocation
    PlayerVocation includes player specific stats and Warrior has no
    vocation specific behavior yet.

---
+++ resources/characters/vocations/player_vocation.gd
@@ -0,0 +1,9 @@
+class_name PlayerVocation
+extends Vocation
+## Base resource for storing player stats
+
+@export_group("Player Stats")
+## Max stamina per turn in card battles
+@export var max_stamina: int = 0
+## Max hand size per turn in card battles
+@export var max_hand_size: int = 0
