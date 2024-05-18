863c2afb1e5dc08f2fd3ebbfb9765f98ae1bdf84
Jover Lee <joverlee521@gmail.com>
2024-05-18 16:43:37 -0700

    Character: make health and defense private variables

--- resources/characters/character.gd
+++ resources/characters/character.gd
@@ -2,9 +2,9 @@ class_name Character
 extends Resource
 ## Base resource for storing and calculating character stats
 
-## Emitted whenever [memeber Character.health] changes in value
+## Emitted whenever [memeber Character._health] changes in value
 signal health_changed(new_value: int)
-## Emitted whenever [member Character.defense] changes in value
+## Emitted whenever [member Character._defense] changes in value
 signal defense_changed(new_value: int)
 
 ## Character's vocation with basic stats
@@ -14,51 +14,51 @@ signal defense_changed(new_value: int)
 @export var cards: Array[CardAttributes] = []
 
 ## Character's current defense
-var defense: int = 0:
+var _defense: int = 0:
 	set(num):
-		defense = num
+		_defense = num
 		defense_changed.emit(num)
 ## Character's current health
-var health: int = 0:
+var _health: int = 0:
 	set(val):
-		health = val
+		_health = val
 		health_changed.emit(val)
 
 
 ## Initialize the following stats: [br]
-## 1. [member Character.health] to [member Vocation.max_health]
-## 2. [member Character.defense] to [member Vocation.starting_defense]
+## 1. [member Character._health] to [member Vocation.max_health]
+## 2. [member Character._defense] to [member Vocation.starting_defense]
 ## 3. If [member Character.cards] is empty, then use [member Vocation.cards]
 func init_character_stats() -> void:
-	health = vocation.max_health
-	defense = vocation.starting_defense
+	_health = vocation.max_health
+	_defense = vocation.starting_defense
 	if cards.is_empty():
 		cards = vocation.cards.duplicate(true)
 
 
 func remove_all_defense() -> void:
-	defense = 0
+	_defense = 0
 
 
 func add_defense(num: int) -> void:
-	defense += num
+	_defense += num
 
 
 func add_health(num: int) -> void:
-	health += num
+	_health += num
 
 
-## Subtract [param damage] from [member Character.defense] and if there
-## is remaining damage, then subtract from [member Character.health]
-## Subtracts directly from [member Character.health] if [param ignore_defense]
+## Subtract [param damage] from [member Character._defense] and if there
+## is remaining damage, then subtract from [member Character._health]
+## Subtracts directly from [member Character._health] if [param ignore_defense]
 ## is set to true.
 func take_damage(damage: int, ignore_defense: bool = false) -> void:
 	if not ignore_defense:
-		if damage > defense:
-			damage -= defense
-			defense = 0
+		if damage > _defense:
+			damage -= _defense
+			_defense = 0
 		else:
-			defense -= damage
+			_defense -= damage
 			damage = 0
 
-	health -= damage
+	_health -= damage
