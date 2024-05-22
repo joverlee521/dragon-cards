53e1a8a650b1ea9d5e869c7e7a09a3026c46850a
Jover Lee <joverlee521@gmail.com>
2024-05-21 20:57:53 -0700

    Add CardBattle scene with player stats

---
+++ scenes/card_battle/card_battle.gd
@@ -0,0 +1,24 @@
+class_name CardBattle
+extends Node
+## The root scene for the card battle
+
+## The player's [Character] resource
+@export var player: Character = Character.new()
+## Flag to init [member CardBattle.player] character stats on ready
+@export var init_player_stats: bool = false
+
+
+func _ready() -> void:
+	if init_player_stats:
+		player.init_stats()
+	start_battle()
+
+
+## Sets the scene to start of battle
+func start_battle() -> void:
+	_set_player_stats_labels()
+
+
+func _set_player_stats_labels() -> void:
+	$PlayerStats/Health.text = "%s / %s" % [str(player._health), str(player.vocation.max_health)]
+	$PlayerStats/Defense.text = str(player._defense)
