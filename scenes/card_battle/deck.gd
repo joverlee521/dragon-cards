0d65e4f76d03f31773fd7969353f1579e1369dc3
Jover Lee <joverlee521@gmail.com>
2024-05-25 15:03:45 -0700

    Deck: refactor + clean up code

--- scenes/card_battle/deck.gd
+++ scenes/card_battle/deck.gd
@@ -2,42 +2,76 @@ class_name Deck
 extends Node2D
 ## Base deck for holding [CardAttributes] that can instantiate [Card]s
 
+# Signals ##########################################################################################
+
+
+
+# Enums ############################################################################################
+
+
+
+# Constants ########################################################################################
+
+
+
+# @export variables ################################################################################
+
 @export_group("Deck Visual")
 @export var deck_name: String = ""
 ## The image of the deck when [member Deck.cards] is not empty
 @export var image: CompressedTexture2D
 
+# Public variables #################################################################################
+
+
+
+# Private variables ################################################################################
+
 ## The [CardAttributes] currently in the deck
 var _cards: Array[CardAttributes] = []
 
+# @onready variables ###############################################################################
 
+
+
+# Optional _init method ############################################################################
+
+
+
+# Optional _enter_tree() method ####################################################################
+
+
+
+# Optional _ready method ###########################################################################
 func _ready() -> void:
-	if deck_name:
-		$DeckName.text = deck_name
+	$DeckName.text = deck_name
 	$DeckImage.texture = image
 	_update_displays()
 
 
-## Returns whether the [member Deck._cards] is empty
+# Optional remaining built-in virtual methods ######################################################
+
+
+
+# Public methods ###################################################################################
+
 func is_empty() -> bool:
 	return _cards.is_empty()
 
 
-## Shuffles the [member Deck._cards]
 func shuffle() -> void:
 	_cards.shuffle()
 
 
-## Add [param card] to the [member Deck._cards]
 func add_card(card: CardAttributes) -> void:
 	_cards.append(card)
 	_update_displays()
 
 
-## Remove all [CardAttributes] from [member Deck._cards]
 func remove_all_cards() -> Array[CardAttributes]:
-	var removed_cards = _cards.duplicate(true)
+	var removed_cards: Array[CardAttributes] = _cards.duplicate(true)
 	_cards = []
+	_update_displays()
 	return removed_cards
 
 
@@ -55,8 +89,12 @@ func replace_cards(new_cards: Array[CardAttributes]) -> void:
 	_update_displays()
 
 
+# Private methods ##############################################################
+
 ## Match the displayed card count to the number of [CardAttributes] in [member Deck._cards].
 ## Only display deck image if there are [member Deck._cards] is not empty
 func _update_displays() -> void:
 	$CardCount.text = str(len(_cards))
 	$DeckImage.visible = not _cards.is_empty()
+
+# Subclasses ###################################################################
