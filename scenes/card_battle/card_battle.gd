186913797458ea7433b7484440e347dbe93bb7c6
Jover Lee <joverlee521@gmail.com>
2024-05-25 17:12:57 -0700

    CardBattle: Refactor + code cleanup

--- scenes/card_battle/card_battle.gd
+++ scenes/card_battle/card_battle.gd
@@ -2,18 +2,33 @@ class_name CardBattle
 extends Node
 ## The root scene for the card battle
 
+# Signals ##########################################################################################
+
+
+
+# Enums ############################################################################################
+
+
+
+# Constants ########################################################################################
+
+
+
+# @export variables ################################################################################
+
+@export_group("Action Timers and Scale")
 ## Time delayed at the start of the battle before the first hand is dealt
-const START_BATTLE_DELAY: float = 0.5
+@export var START_BATTLE_DELAY: float = 0.5
 ## Time delayed between each card that is dealt
-const DEAL_CARD_DELAY: float = 0.3
+@export var DEAL_CARD_DELAY: float = 0.3
 ## Time delayed between each card that is played
-const PLAYED_CARD_DELAY: float = 0.7
+@export var PLAYED_CARD_DELAY: float = 0.7
 ## Final scale of played card
-const PLAYED_CARD_SCALE: Vector2 = Vector2(1.3, 1.3)
+@export var PLAYED_CARD_SCALE: Vector2 = Vector2(1.3, 1.3)
 ## Time delayed between each card that is discarded
-const DISCARD_CARD_DELAY: float = 0
+@export var DISCARD_CARD_DELAY: float = 0
 
-
+@export_group("Battle Setup")
 ## The player's [Character] resource
 @export var player: Character = Character.new()
 ## Flag to init [member CardBattle.player] character stats on ready
@@ -23,7 +38,27 @@ const DISCARD_CARD_DELAY: float = 0
 ## The [Enemy]s to fight in card battle
 @export var enemies: Array[PackedScene] = [] # Array[Enemy]
 
+# Public variables #################################################################################
 
+
+
+# Private variables ################################################################################
+
+
+
+# @onready variables ###############################################################################
+
+
+
+# Optional _init method ############################################################################
+
+
+
+# Optional _enter_tree() method ####################################################################
+
+
+
+# Optional _ready method ###########################################################################
 func _ready() -> void:
 	_connect_player_stats_signals()
 	if init_player_stats:
@@ -31,7 +66,12 @@ func _ready() -> void:
 	start_battle()
 
 
-## Sets the scene to start of battle
+# Optional remaining built-in virtual methods ######################################################
+
+
+
+# Public methods ###################################################################################
+
 func start_battle() -> void:
 	_update_player_health_label()
 	_update_player_defense_label()
@@ -42,10 +82,6 @@ func start_battle() -> void:
 	start_player_turn()
 
 
-## Starts the player turn by
-## [br] 1. Setting the player hand with max stamina
-## [br] 2. Dealling a full hand of [Card]s
-## [br] 3. Disabling the PlayCard button
 func start_player_turn() -> void:
 	$PlayerControls/PlayCard.disabled = true
 	$PlayerControls/EndTurn.disabled = false
@@ -67,6 +103,7 @@ func deal_cards(num: int) -> void:
 		await get_tree().create_timer(DEAL_CARD_DELAY).timeout
 
 
+## Discard [Card] by adding to the DiscardDeck
 func discard_card(card: Card) -> void:
 	if card.get_parent() == self:
 		remove_child(card)
@@ -78,11 +115,15 @@ func start_enemies_turn() -> void:
 	$PlayerControls/PlayCard.disabled = true
 	$PlayerControls/EndTurn.disabled = true
 	$PlayerHand.set_cards_clickable(false)
+
 	for enemy in get_tree().get_nodes_in_group($EnemyManager.ENEMIES_IN_BATTLE):
 		await _play_enemy_card(enemy.get_next_card())
+
 	get_tree().call_group($EnemyManager.ENEMIES_IN_BATTLE, "pick_next_card")
 	start_player_turn()
 
+
+# Private methods ##################################################################################
 
 ## Connects the player's emitted stats signals to the label updates
 func _connect_player_stats_signals() -> void:
@@ -90,17 +131,14 @@ func _connect_player_stats_signals() -> void:
 	player.defense_changed.connect(_update_player_defense_label)
 
 
-## Updates the text of player's health label
 func _update_player_health_label(player_health: int = player._health) -> void:
 	$PlayerStats/Health.text = "%s / %s" % [str(player_health), str(player.vocation.max_health)]
 
 
-## Updates the text of the player's defense label
 func _update_player_defense_label(player_defense: int = player._defense) -> void:
 	$PlayerStats/Defense.text = str(player_defense)
 
 
-## Updates the text of the player's stamina label
 func _update_player_stamina_label(player_stamina: int = player.vocation.max_stamina) -> void:
 	$PlayerStats/Stamina.text = "%s / %s" % [str(player_stamina), str(player.vocation.max_stamina)]
 
@@ -136,3 +174,6 @@ func _play_enemy_card(card_attributes: CardAttributes) -> void:
 	card.set_clickable(false)
 	await _play_card(card)
 	card.queue_free()
+
+
+# Subclasses #######################################################################################
